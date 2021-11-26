ed88cf70ceab52df6eae531299ad6dd954ab0473
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-11-26 21:08:28 +0700

    add component to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "components/header";
+import Navbar from "components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 h-screen w-full">
+      <Header />
+      <Navbar />
     </div>
   );
 }
