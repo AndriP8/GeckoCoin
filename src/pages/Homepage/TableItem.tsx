b5be376d1fe899620f7190b3514968c341bb9ad8
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-12-01 20:01:08 +0700

    update style and data fetching

--- src/pages/Homepage/TableItem.tsx
+++ src/pages/Homepage/TableItem.tsx
@@ -1,23 +1,13 @@
-import { FaRegStar } from "react-icons/fa";
 import NumberFormat from "react-number-format";
+import { FaRegStar } from "react-icons/fa";
 import { MarketCapTypes } from "types";
 import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchMarketCap = () => {
-  return axios.get(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=2&page=1&price_change_percentage=1h%2C24h%2C7d"
-  );
-};
+import { getMarketCap } from "service/homepage";
 
 function TableItem() {
-  const { isLoading, data } = useQuery("market_cap", fetchMarketCap, {
-    refetchInterval: 600000,
+  const { data } = useQuery("market_cap", () => getMarketCap(), {
+    refetchInterval: 30000,
   });
-
-  if (isLoading) {
-    return <h1>Loading.....</h1>;
-  }
 
   const Percentage = ({ percent }: { percent: any }) => {
     let formatPercent: any;
@@ -32,11 +22,11 @@ function TableItem() {
 
   return (
     <div className="overflow-x-auto px-4 h-screen">
-      <table className="table w-full">
-        <thead className="">
-          <tr>
-            <th>#</th>
-            <th>Coin</th>
+      <table className="table w-full text-right">
+        <thead>
+          <tr className="border-b-2">
+            <th className="text-center w-[2%]">#</th>
+            <th className="text-left w-1/5">Coin</th>
             <th>Price</th>
             <th>1h</th>
             <th>24h</th>
@@ -46,20 +36,24 @@ function TableItem() {
           </tr>
         </thead>
         <tbody>
-          {data?.data.map((item: MarketCapTypes) => (
-            <tr key={item.id} className="hover">
+          {data?.data.map((item: MarketCapTypes, idx: number) => (
+            <tr key={item.id} className="hover bg-white">
               <th className="flex items-center">
                 <FaRegStar />
-                <span className="pl-2">{1}</span>
+                <span className="pl-2 font-normal">{idx + 1}</span>
               </th>
               <td>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center mr-12">
+                <div className="flex items-center">
+                  <div className="lg:flex lg:items-center lg:w-4/5 w-max text-left">
                     <img src={item.image} className="w-5 h-5" alt="Coin-name" />
-                    <span className="font-bold ml-4">{item.name}</span>
+                    <span className="font-bold ml-4 whitespace-normal hidden lg:inline">
+                      {item.name}
+                    </span>
                   </div>
                   <div>
-                    <p className="uppercase text-left pr-2">{item.symbol}</p>
+                    <p className="uppercase text-left font-bold lg:font-normal ml-2">
+                      {item.symbol}
+                    </p>
                   </div>
                 </div>
               </td>
