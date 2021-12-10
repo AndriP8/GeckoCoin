ddec50a6e2b1f20dfc21d57568e98340628007e9
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-12-10 09:54:07 +0700

    update component homepage

---
+++ src/pages/Homepage/MarketCap.tsx
@@ -0,0 +1,34 @@
+import { FiCornerRightDown } from "react-icons/fi";
+
+const MarketCap = () => {
+  return (
+    <div className="px-4">
+      <div className="flex items-center justify-start py-4">
+        <h3 className="text-gray-700">Cryptocurrency Prices by Market Cap</h3>
+      </div>
+      <div>
+        <p className="text-sm text-gray-500">
+          The global cryptocurrency market cap today is $2.58 Trillion, a -7.8%{" "}
+          <span>
+            <FiCornerRightDown className="inline-flex text-red-500 -ml-1" />
+          </span>{" "}
+          change in the last 24 hours.{" "}
+          <span className="underline"> Read More</span>
+        </p>
+      </div>
+      <div className="pt-4 pb-3 border-b border-gray-300">
+        <div className="btn btn-sm btn-outline btn-accent rounded-full py-1 px-4 border-gray-300">
+          <button>USD</button>
+        </div>
+        <div className="btn btn-sm btn-outline btn-accent rounded-full py-1 px-4 border-gray-300 mx-3 capitalize">
+          <button>All Categories</button>
+        </div>
+        <div className="btn btn-sm btn-outline btn-accent rounded-full py-1 px-4 border-gray-300 capitalize">
+          <button>Filter</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketCap;
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,6 +1,6 @@
 import Page from "components/layout";
 import NavbarTabs from "components/navbarTabs";
-import { FiCornerRightDown } from "react-icons/fi";
+import MarketCap from "./MarketCap";
 import Summary from "./Summary";
 import TableItem from "./TableItem";
 import TrendingCoin from "./TrendingCoin";
@@ -9,34 +9,7 @@ function Homepage() {
   return (
     <Page>
       <NavbarTabs />
-      <div className="px-4">
-        <div className="flex items-center justify-start py-4">
-          <h3 className="text-gray-700">Cryptocurrency Prices by Market Cap</h3>
-          <span className="px-3 mt-1">Show Stats</span>
-        </div>
-        <div>
-          <p className="text-sm text-gray-500">
-            The global cryptocurrency market cap today is $2.58 Trillion, a
-            -7.8%{" "}
-            <span>
-              <FiCornerRightDown className="inline-flex text-red-500 -ml-1" />
-            </span>{" "}
-            change in the last 24 hours.{" "}
-            <span className="underline"> Read More</span>
-          </p>
-        </div>
-        <div className="pt-4 pb-3 border-b border-gray-300">
-          <div className="btn btn-sm btn-outline btn-accent rounded-full py-1 px-4 border-gray-300">
-            <button>USD</button>
-          </div>
-          <div className="btn btn-sm btn-outline btn-accent rounded-full py-1 px-4 border-gray-300 mx-3 capitalize">
-            <button>All Categories</button>
-          </div>
-          <div className="btn btn-sm btn-outline btn-accent rounded-full py-1 px-4 border-gray-300 capitalize">
-            <button>Filter</button>
-          </div>
-        </div>
-      </div>
+      <MarketCap />
       <TableItem />
       <TrendingCoin />
       <Summary />
