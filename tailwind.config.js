549ebe2841daa350fe1cfbf3de4daf8d00416c84
Andri Purnomo <56879512+AndriP8@users.noreply.github.com>
2021-12-27 20:23:55 +0700

    update react-script for compatible tailwind v.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         "green-light": "#8dc647",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
